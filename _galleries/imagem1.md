54c7d6d601f872b4139ec731a05666079a8b88aa
betodasilva <jrs_robert@hotmail.com>
2017-08-25 13:33:46 -0300

    Adjusments for images in galleries

--- _galleries/imagem1.md
+++ _galleries/imagem1.md
@@ -1,12 +1,4 @@
 ---
-image: !ruby/object:Dato::Local::FieldType::Image
-  path: "/3100/1503665929-screenshot-from-2017-08-25-09-24-18.png"
-  format: png
-  size: 47507
-  imgix_host: www.datocms-assets.com
-  width: 586
-  height: 598
-  alt: 
-  title: 
+image: https://www.datocms-assets.com/3100/1503676130-screenshot-from-2017-08-25-09-24-18.png?w=580&h=520&fit=crop
 ---
 
--- dato.config.rb
+++ dato.config.rb
@@ -2,7 +2,7 @@ directory "_galleries" do
     dato.galleries.each do |item|
       create_post "#{item.title.parameterize}.md" do
         frontmatter :yaml, {
-          image: item.imagem
+          image: item.imagem.url(w: 580, h: 520, fit: :crop)
         }
       end
     end
@@ -12,8 +12,9 @@ directory "_videos" do
   dato.videos.each do |item|
     create_post "#{item.title.parameterize}.md" do
       frontmatter :yaml, {
-        video: item.video
+        video: item.video.url(w: 854, h:480)
       }
     end
   end
 end
+
